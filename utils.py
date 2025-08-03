7159021a8bbf486cb18dc6dead496448bc8d3553
praneethchalapati <chalapatipraneeth@gmail.com>
2025-08-03 11:06:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ cfg = RunnableConfig(recursion_limit=100)
 
 def initialize_model(google_api_key: str) -> ChatGoogleGenerativeAI:
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         google_api_key=google_api_key
     )
 
